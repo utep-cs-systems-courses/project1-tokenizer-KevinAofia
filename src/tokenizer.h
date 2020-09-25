678a908d3b70982648811ee4b2113cf06c1d3bb8
KevinAofia <aofia21@yahoo.com>
2020-09-25 02:07:03 -0600

    fixed word_start to *word_start.

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,6 +1,8 @@
 #ifndef _TOKENIZER_
 #define _TOKENIZER_
 
+//Prints all options out the user
+void print_options();
 
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
@@ -15,7 +17,7 @@ int non_space_char(char c);
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char word_start(char *str); 
+char *word_start(char *str); 
 
 /* Returns a pointer terminator char following *word */
 char *word_terminator(char *word);
