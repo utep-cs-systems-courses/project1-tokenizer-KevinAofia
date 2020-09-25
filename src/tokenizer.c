01a00f2cea3b501a70c58af0752c92f2947839c5
KevinAofia <aofia21@yahoo.com>
2020-09-24 18:07:35 -0600

    Copied over funtions from the tokenizer.h file and defined method in my own words, also added a test case in space_char to see if I am importing the right files to my UI.main file for future test cases.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,3 +1,43 @@
-//#include <stdio.h>
+#include <stdio.h>
+#include "tokenizer.h" 
+//Functions are defined here from the header file tokenizer.h
+//NOTE:Zero terminators are not printable (therefore false) */
+
+//Return true(non-zero) if char c is a whitespace character('\t' or ' ' is a whitespace character)
+int space_char(char c)
+{
+  if (c == 1)
+    {
+      printf("c is a whitespace character");
+    }
+}
+
+//Return true(non zero) if char c is not a whitespace character(aka non whitespace character)
+int non_space_char(char c);
+
+
+//Returns a pointer to the first non whitespace after a terminated word(aka return a pointer that
+// points to the first character of a next word in a string
+char word_start(char *str); 
+
+//Returns a pointer terminator char following the end of a word(*word)
+char *word_terminator(char *word);
+
+//Counts the number of words in the string argument
+int count_words(char *str);
+
+//Returns a fresly allocated new zero-terminated string containing,<len> chars from <inStr>
+char *copy_str(char *inStr, short len);
+
+//Returns a freshly allocated zero terminated vector of freshly allocated space-separated tokens
+char **tokenize(char* str);
+
+// Prints all tokens.
+void print_tokens(char **tokens);
+
+//Frees all tokens and the vector containing themx.
+void free_tokens(char **tokens);
+
+#endif
 
 
