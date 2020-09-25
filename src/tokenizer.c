99def0f0485b45406d7bcb24a7ed897db11def18
KevinAofia <aofia21@yahoo.com>
2020-09-25 17:33:00 -0600

    update

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h" 
 //Functions are defined here from the header file tokenizer.h
 //NOTE:Zero terminators are not printable (therefore false) */
@@ -13,39 +14,33 @@ void print_options()
 //Return true(non-zero) if char c is a whitespace character('\t' or ' ' is a whitespace character)
 int space_char(char c)
 {
-  if (c == '\t' || c == ' ')
-    {
-      return 1; //True
-    }
-  else
-    {
-      return 0; //False
-    }
+  if (c == '\t' || c == ' ') {
+    return 1; //True
+  }
+  else {
+    return 0; //False
+  }
 }
   
 //Return true(non zero) if char c is not a whitespace character(aka non whitespace character)
 int non_space_char(char c)
 {
-  if(c != '\t' && c != ' ')
-    {
-      return 1; //True
-    }
-  else
-    {
-      return 0; //False
-    }
+  if(c != '\t' && c != ' ') {
+    return 1; //True
+  }
+  else {
+    return 0; //False
+  }
 }
-
 
 //Returns a pointer to the first non whitespace after a terminated word(aka return a pointer that
 // points to the first character of a next word in a string
 char *word_start(char *str)
 {
   int i = 0; //dereference at index 0 of an array
-  while (non_space_char(str[i]) != 1) //while character at index i is not a non whitespace
-    {
-      i++; //check next character
-    }
+  while (non_space_char(str[i]) != 1) { //while character at index i is not a non whitespace
+    i++; //check next character
+  }
   //printf("%c\n",str[i]); //uncomment to test
   return &str[i]; //pointer to str at [i]
 }
@@ -54,10 +49,9 @@ char *word_start(char *str)
 char *word_terminator(char *word)
 {
   int i = 0;
-  while (space_char(word[i]) != 1)
-    {
-      i++;
-    }
+  while (space_char(word[i]) != 1) {
+    i++;
+  }
   //printf("End of word found at index: %d\n",i);
   return &word[i]; //pointer to word at [i]
 }
@@ -65,16 +59,18 @@ char *word_terminator(char *word)
 //Counts the number of words in the string argument
 int count_words(char *str)
 {
-  int count = 1;
+  int count = 0;
   char *p1 = word_start(str);
   char *p2 = word_terminator(p1);
-  //printf(p2);
-  while(p2[0] != '\0')
-    {
-      count++;
-      p1 = word_start(p2);
-      p2 = word_terminator(p1);
+  
+  while(non_space_char(*p1) == 1){
+    count++;
+    p2 = word_terminator(p1);
+    if ((*p2) == '\0'){
+      break;
     }
+    p1 = word_start(p2);
+  }
   return count;
 }
 
