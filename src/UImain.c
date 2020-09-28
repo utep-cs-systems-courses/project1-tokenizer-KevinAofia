3590aa06292cc1940bd577fa1ff04d379f6869bb
KevinAofia <aofia21@yahoo.com>
2020-09-27 21:59:40 -0600

    last change

--- src/UImain.c
+++ src/UImain.c
@@ -4,9 +4,8 @@
 
 int main()
 {
-  print_options();
-  
-  /*char test1 = '\0';
+  /*
+  char test1 = '\0';
   char test2 = ' ';
   char *test3 = "Hello to all.";
   char *test4 = "My name is Kev.";
@@ -15,7 +14,7 @@ int main()
   printf("Test 3: %c :\n", *word_start(test3)); //return an address, so need to dereference
   printf("Test 4: %c :\n", *word_terminator(test4)); //returns an address, so need to dereference
   printf("Test 5a: %d :\n", count_words(test3));
-  printf("Test 5b: %d :\n", count_words(test4));*/
+  printf("Test 5b: %d :\n", count_words(test4));
   char input[1000];
   char c;
   int i = 0;
@@ -25,22 +24,34 @@ int main()
     c = getchar();
     i++;
   }
-  
   char *copied;
   copied = copy_str(input,i);
-
   printf("Copied: %s\n", copied);
-  /*
-  while(*copied != '\0') {
-    printf("character in array: %c \n", *copied);
-    copied++;
-  }
+  char **tester = tokenize(copied);
+  print_tokens(tester);
+  free_tokens(tester);
+  print_tokens(tester);
   */
-  char **pw = tokenize(copied);
-  print_tokens(pw);
-  free_tokens(pw);
-  print_tokens(pw);
 
+  char input[240];
+  char c;
+  int num;
+  int i = 0;
+  char *copied;
   
+  printf("Please select an option:\n");
+  print_options();
+  printf("Tokenizing > ");
+  c = getchar();
+  while (c != '\n') {
+    input[i] = c;
+    c = getchar();
+    i++;
+  }
+  printf("\n");
+  copied = copy_str(input,i);
+  char **tokenized = tokenize(copied);
+  print_tokens(tokenized);
+  free_tokens(tokenized);
 }
 
