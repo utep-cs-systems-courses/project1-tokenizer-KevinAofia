c120fc532821a931a64aee8cb1f96ca2225e20fb
KevinAofia <aofia21@yahoo.com>
2020-10-03 20:16:22 -0600

    updated after demo

--- src/history.c
+++ src/history.c
@@ -1,15 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "history.h"
 #include "tokenizer.h"
 
 
-List* init_history();
+List *init_history() {
+  List *history = malloc(sizeof(List)); //allocate memory for a new struct List
+  (*history).root = NULL; //starting root node is set to NULL
+  //history->root = NULL;
+  return history;
+}
 
-void add_history(List *list, char *str);
+void add_history(List *list, char *str) {
+  return;
+}
 
 char *get_history(List *list, int id) ;
 
 
-void print_history(List *list) ;
+void print_history(List *list) {
+  Item *temp = (*list).root;
+  printf("\t*****History*****\n");
+  while(temp != NULL) {
+    printf("ID: %d\nString: %s\n",(*temp).id,(*temp).str);
+    //printf("ID: %d\nString: %s\n",temp->id,temp->str); //equivalent to line above
+    temp = (*temp).next;
+  }
+  printf("\t*****End of history*****\n");
+}
 
-
-void free_history(List *list) ;
+void free_history(List *list) {
+  Item *temp = (*list).root;
+  Item *temp2;
+  while(temp != NULL) { //while element present in linked list
+    temp2 = temp;
+    free(temp2);
+    temp = (*temp).next; //store temp to the next element    
+  }
+}
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
-
-//Functions are defined here from the header file tokenizer.h
-//NOTE:Zero terminators are not printable (therefore false) */
+#include "history.h"
 
 //Return true(non-zero) if char c is a whitespace character('\t' or ' ' is a whitespace character)
 int space_char(char c)
@@ -22,6 +20,7 @@ int non_space_char(char c)
   }
   return 0; //must be a whitespace character
 }
+
 //Returns a pointer to the first non whitespace after a terminated word(aka return a pointer that
 // points to the first character of a next word in a string
 char *word_start(char *str)
@@ -71,13 +70,19 @@ int count_words(char *str)
 //Returns a freshly allocated new zero-terminated string containing,<len> chars from <inStr>
 char *copy_str(char *inStr, short len)
 {
-  char *copy = malloc( (len + 1) * sizeof(char)); //allocate memory (add 1 for \0 character) 
+  if (space_char(*inStr)) { //if string starts with a space
+    while(space_char(*inStr)) { //while spaces are present
+      inStr++; //move by one address
+      len--; //subtract from the original length count passed
+    }
+  }
+  char *copy = malloc((len + 1) * sizeof(char)); //allocate memory (add 1 for \0 character) 
   int i = 0;
   while (i < len) { //while index is less than length of passed array
     copy[i] = inStr[i]; //this is equivalent to saying *copy = *inStr
     i++; //increment index for next character in string
   }
-  copy[i+1] = '\0'; //set next address value to terminating character
+  copy[i] = '\0'; //set next address value to terminating character
   return copy;
 }
 
@@ -95,7 +100,7 @@ char **tokenize(char *str) {
     endingPtr = word_terminator(startingPtr);
     i++;
   }
-  tokens[i+1] = '\0'; //put term character in last array
+  tokens[i] = '\0'; //put term character in last array
   //printf("Done\n");
   return tokens;
 }
@@ -116,13 +121,11 @@ void print_tokens(char **tokens)
 //Frees all tokens and the vector containing themx.
 void free_tokens(char **tokens)
 {
-  /*
   int i = 0;
   while(tokens[i] != NULL) {
-    free(tokens[i]); //free the heap memory
+    free(tokens[i]); //free what second layer
     i++;
   }
-  */
-  free(tokens);
+  free(tokens); //free the top layer pointing at second layer
   printf("All tokens are freed.\n");
 }
